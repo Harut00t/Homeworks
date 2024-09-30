d2f5ecc897d5d7e56d3b2f186fc2f2f2637c8e57
Seiichi Aiko <seiichipaiko@iskolarngbayan.pup.edu.ph>
2024-09-30 10:17:03 +0800

    Encryption of the text
    This code encrypts the message by getting the word-by-word arrangement of the variable 'key' to the input text. To enumerate,
    suppose that 123 is the list of the variable 'chars' and the shuffled variable 'key' is abc. When the input typed by the user is '123' then the encrypted text will be abc.

--- Encrypt_and_Decrypt_texts.py
+++ Encrypt_and_Decrypt_texts.py
@@ -8,5 +8,16 @@ key = chars.copy()
 
 random.shuffle(key)
 
-print(f"chars: {chars}")
-print(f"key: {key}")
+#print(f"chars: {chars}")
+#print(f"key: {key}")
+
+#Encryption
+plain_text = input("Please enter a message to encrypt: ")
+cipher_text = ""
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"original message : {plain_text}")
+print(f"encrypted message : {cipher_text}")
